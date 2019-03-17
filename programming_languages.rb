3e6e1c03d797c1825e431cfada51b90d5697e3fe
Pete A <peter.ayresmusic@gmail.com>
2019-03-17 21:40:10 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,6 @@ require'pry'
 def reformat_languages(languages_by_style)
   new_hash = {}
   languages_by_style.each do |language,lang_data|
-binding.pry
     if language == :ruby
       new_hash[:ruby] = {:type => "interpreted",:style => [:oo]}
       puts new_hash
