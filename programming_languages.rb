33087f2c50489f205603dd9c802019130ff1dd40
Pete A <peter.ayresmusic@gmail.com>
2019-03-17 21:38:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
+#iterate over the old hash and build a new one with new_hash
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |language,lang_data|
+    if language == :ruby
+      new_hash[:ruby] = {:type => "interpreted",:style => [:oo]}
+      puts new_hash
+    end
+  end
 end
