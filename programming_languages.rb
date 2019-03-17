d7c81c9250abef2a798b5cdcdd659c2ad42e01bf
Pete A <peter.ayresmusic@gmail.com>
2019-03-17 22:01:11 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -12,6 +12,8 @@ def reformat_languages(languages_by_style)
       new_hash[:javascript] = {:type => "interpreted",:style => [:oo, :functional]}
     elsif style == :python
       new_hash[:python] = {:type => "interpreted",:style => [:oo]}
+    elsif style == :java
+      new_hash[:java] = {:type => "compiled",:style => [:oo]}
       return new_hash
     end
   end
